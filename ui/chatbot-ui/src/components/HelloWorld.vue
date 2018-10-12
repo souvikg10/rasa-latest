4837ee580944f3a1c5283053d30e1f3aa7f428f0
Souvik Ghosh <souvik_g10@yahoo.com>
2018-10-12 15:19:24 +0200

    Initialize UI chatbot - WIP

---
+++ ui/chatbot-ui/src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+  <h2>{{msg}}</h2>
+  <div>
+
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
