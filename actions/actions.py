4605b88abbee57e2745515fcdad9c81d6977a825
Souvik Ghosh <souvik_g10@yahoo.com>
2018-10-30 21:55:02 +0100

    removed ui

--- actions/actions.py
+++ actions/actions.py
@@ -5,18 +5,9 @@ from rasa_core_sdk.events import SlotSet
 class ActionSetTicket(Action):
    def name(self):
       # type: () -> Text
-      return "set_ticket_number"
+      return "card_lost"
 
    def run(self, dispatcher, tracker, domain):
       # type: (Dispatcher, DialogueStateTracker, Domain) -> List[Event]
-        Number = tracker.get_slot("number")
-        return [SlotSet("Ticket", Number)]
+        dispatcher.utter_message("I have contacted the team. We will issue you a new card really soon")
 
-class ActionRetrieveTicket(Action):
-   def name(self):
-      # type: () -> Text
-      return "retrieve_ticket_status"
-
-   def run(self, dispatcher, tracker, domain):
-      # type: (Dispatcher, DialogueStateTracker, Domain) -> List[Event]
-        dispatcher.utter_message("Votre ticket n'est pas disponible")
